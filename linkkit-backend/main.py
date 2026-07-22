89f2ea27868cbd7fc9c75a6f190d0e766728a00c
pawel201 <pawelkumar000@gmail.com>
2026-07-22 18:38:45 +0530

    Added Instagram Follow-Gated Checkbox feature

--- linkkit-backend/main.py
+++ linkkit-backend/main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # Sabhi websites ko allow karega
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
